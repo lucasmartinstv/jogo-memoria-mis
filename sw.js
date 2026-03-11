56bc0fcdf26546f8d2d0a42eb35ccda8dc36d29c
lucasmartinstv <lmartins.ce@gmail.com>
2026-03-11 14:14:11 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,29 @@
-const CACHE_NAME = 'mis-memoria-v2';
+const CACHE_NAME = 'mis-memoria-v3';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  './imagens/logo_app.png',
+  './imagens/logo_ini.jpg',
+  './imagens/capa.jpg'
+];
+
+for (let i = 2; i <= 40; i++) {
+  assets.push(`./imagens/${i}.jpg`);
+}
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(assets)));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});const CACHE_NAME = 'mis-memoria-v2';
 
 // Lista de ficheiros principais
 const assets = [
@@ -41,3 +66,4 @@ self.addEventListener('fetch', event => {
     })
   );
 });
+
