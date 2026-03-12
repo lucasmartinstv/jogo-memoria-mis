6a382b6701bb56d0974a00feef477c05d07c6408
lucasmartinstv <lmartins.ce@gmail.com>
2026-03-12 11:02:03 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mis-memoria-v3';
+const CACHE_NAME = 'mis-memoria-v5';
 const ASSETS = [
   './',
   './index.html',
@@ -8,22 +8,20 @@ const ASSETS = [
   './imagens/logo_ini.jpg'
 ];
 
-// Adiciona automaticamente as imagens do jogo (2.jpg até 40.jpg)
+// Adiciona as imagens das cartas (2 a 40)
 for (let i = 2; i <= 40; i++) {
   ASSETS.push(`./imagens/${i}.jpg`);
 }
 
-// Instalação do Cache
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      console.log('Cacheando arquivos do MIS...');
+      console.log('MIS: Cacheando recursos para TV 75...');
       return cache.addAll(ASSETS);
     })
   );
 });
 
-// Limpeza de cache antigo ao atualizar versão
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((keys) => {
@@ -34,7 +32,6 @@ self.addEventListener('activate', (event) => {
   );
 });
 
-// Estratégia: Cache First (Prioriza offline para o totem)
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
